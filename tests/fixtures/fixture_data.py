bd72652a9fedf81f1ccc40673e2d0178ac8b7569
Vladislav Konstantinov <vk.konstantunov@yandex.ru>
2022-10-21 17:15:58 +0300

    Revie 2

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -2,7 +2,7 @@ import tempfile
 
 import pytest
 from mixer.backend.django import mixer as _mixer
-from posts.models import Group, Post
+from posts.models import Post, Group
 
 
 @pytest.fixture()
@@ -20,26 +20,18 @@ def mixer():
 @pytest.fixture
 def post(user):
     image = tempfile.NamedTemporaryFile(suffix=".jpg").name
-    return Post.objects.create(
-        text="Тестовый пост 1", author=user, image=image
-    )
+    return Post.objects.create(text='Тестовый пост 1', author=user, image=image)
 
 
 @pytest.fixture
 def group():
-    return Group.objects.create(
-        title="Тестовая группа 1",
-        slug="test-link",
-        description="Тестовое описание группы",
-    )
+    return Group.objects.create(title='Тестовая группа 1', slug='test-link', description='Тестовое описание группы')
 
 
 @pytest.fixture
 def post_with_group(user, group):
     image = tempfile.NamedTemporaryFile(suffix=".jpg").name
-    return Post.objects.create(
-        text="Тестовый пост 2", author=user, group=group, image=image
-    )
+    return Post.objects.create(text='Тестовый пост 2', author=user, group=group, image=image)
 
 
 @pytest.fixture
@@ -50,8 +42,6 @@ def few_posts_with_group(mixer, user, group):
 
 
 @pytest.fixture
-def another_few_posts_with_group_with_follower(
-    mixer, user, another_user, group
-):
-    mixer.blend("posts.Follow", user=user, author=another_user)
+def another_few_posts_with_group_with_follower(mixer, user, another_user, group):
+    mixer.blend('posts.Follow', user=user, author=another_user)
     mixer.cycle(20).blend(Post, author=another_user, group=group)
--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -3,9 +3,7 @@ import pytest
 
 @pytest.fixture
 def user(django_user_model):
-    return django_user_model.objects.create_user(
-        username="TestUser", password="1234567"
-    )
+    return django_user_model.objects.create_user(username='TestUser', password='1234567')
 
 
 @pytest.fixture
@@ -17,5 +15,4 @@ def user_client(user, client):
 @pytest.fixture
 def another_user(mixer):
     from django.contrib.auth.models import User
-
-    return mixer.blend(User, username="AnotherUser")
+    return mixer.blend(User, username='AnotherUser')
